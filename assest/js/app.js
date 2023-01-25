bd6f576b32da474da416f3624e457e2bccda0b00
Mehdi Ilkhani Nasab <mehdiilkhaninasab7@gmail.com>
2023-01-25 22:33:40 +0330

    add all files

---
+++ assest/js/app.js
@@ -0,0 +1,111 @@
+
+var newCourseSwiper = new Swiper('.new-courses-swiper', {
+  pagination: '.swiper-pagination',
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflow: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true
+  },
+  loop: true
+});
+
+
+var blogBoxSwiper = new Swiper('.blog-swiper', {
+
+  loop: true,
+  grabCursor: true,
+  breakpoints: {
+    350: {
+      slidesPerView: 1,
+      spaceBetween: 5,
+    },
+
+    540: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 7,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 8,
+    },
+  },
+
+});
+
+
+var relatedCoursesSwiper = new Swiper('.related-courses-swiper', {
+
+  loop: true,
+  grabCursor: true,
+  breakpoints: {
+    350: {
+      slidesPerView: 1,
+      spaceBetween: 5,
+    },
+
+    540: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 7,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 8,
+    },
+  },
+
+});
+var studentCommentSwiper = new Swiper('.student-comment-swiper', {
+
+  loop: true,
+  grabCursor: true,
+  breakpoints: {
+    350: {
+      slidesPerView: 1,
+      spaceBetween: 5,
+    },
+
+    540: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 7,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 8,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+});
+
+
+
